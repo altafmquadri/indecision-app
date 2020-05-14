3ade5551ae9c7480e781d18d3e8a8fecb69a6b0f
Altaf Quadri <altafmquadri@gmail.com>
2020-05-14 02:27:02 -0400

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,88 @@
 'use strict';
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    app.options && app.options.length ? React.createElement(
+        'p',
+        null,
+        'Here are your options'
+    ) : React.createElement(
+        'p',
+        null,
+        'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var user = {
+    name: 'Altaf Quadri',
+    hobby: 'Riding a Motorcycle',
+    location: 'New York'
+};
+
+function getLocation(location) {
+    if (location) return React.createElement(
+        'p',
+        null,
+        'Location: ',
+        location
+    );
+}
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.hobby && React.createElement(
+        'p',
+        null,
+        user.hobby
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
 
-/* babel src/app.js --out-file=public/scripts/app.js --presets="env,react" --watch */
+/*
+Instruction in video 9 Setting up Babel
+npm init
+npm i -g babel-cli@6.24.1
+npm i babel-preset-react@6.24.1
+npm i babel-preset-env@1.5.2
+babel src/app.js --out-file=public/scripts/app.js --presets="env,react" --watch */
--- src/app.js
+++ src/app.js
@@ -1,10 +1,51 @@
-var template = <p>Does this change?</p>
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options && app.options.length ?
+            <p>Here are your options</p> :
+            <p>No options</p>}
+
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+)
+
+var user = {
+    name: 'Altaf Quadri',
+    hobby: 'Riding a Motorcycle',
+    location: 'New York'
+}
+
+function getLocation(location) {
+    if (location) return <p>Location: {location}</p>
+}
+
+var template2 = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.hobby && <p>{user.hobby}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
+
+
+
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
 
 
-/* 
+/*
 Instruction in video 9 Setting up Babel
 npm init
 npm i -g babel-cli@6.24.1
