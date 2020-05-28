13f8f863995735039877d20923f4ed494316c4d6
Altaf Quadri <altafmquadri@gmail.com>
2020-05-27 22:54:29 -0400

    ad depencies to make loader for sass-loader and node-sass to work, updated webpack, webpack server, webpack cli

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        },
+    {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+
+    }]
     },
     devtool: 'cheap-module-source-map',
     devServer: {
